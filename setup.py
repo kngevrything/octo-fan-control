8484ab550289e8179f1a98fd75caa37a0cee9899
kngevrything <83468843+kngevrything@users.noreply.github.com>
2022-09-12 14:30:57 -0700

    Update setup.py
    updating parameter info

--- setup.py
+++ setup.py
@@ -80,17 +80,17 @@ except:
     sys.exit(-1)
 
 setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
-    identifier=plugin_identifier,
-    package=plugin_package,
-    name=plugin_name,
-    version=plugin_version,
-    description=plugin_description,
-    author=plugin_author,
-    mail=plugin_author_email,
-    url=plugin_url,
-    license=plugin_license,
-    requires=plugin_requires,
-    additional_packages=plugin_additional_packages,
+    identifier=EnclosureFanController,
+    package=octoprint_EnclosureFanController,
+    name=Enclosure Fan Controllere,
+    version=1.0,
+    description=Plugin that monitors a temperature sensor and ,
+    author=kngevrything,
+    mail=kngevrything@gmail.com,
+    url=https://github.com/kngevrything/octo-fan-control,
+    license=AGPLv3,
+    requires=python-w1thermsensor, GPIO,
+    additional_packages=python-w1thermsensor,
     ignored_packages=plugin_ignored_packages,
     additional_data=plugin_additional_data,
 )
