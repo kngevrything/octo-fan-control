bb764227657856fe5a4abde17dc63ceb14b6384c
KngEVrythINg <kngevrything@gmail.com>
2022-09-17 14:48:24 -0700

    Fixed issues with the JS causing issues with the system menu

--- octoprint_EnclosureFanController/__init__.py
+++ octoprint_EnclosureFanController/__init__.py
@@ -55,7 +55,7 @@ class EnclosureFanController(	octoprint.plugin.StartupPlugin,
 
 	def get_template_configs(self):
 		return [
-			dict(type="navbar",custom_bindings=False),
+#			dict(type="navbar",custom_bindings=False),
 			dict(type="settings", custom_bindings=False)
 			]
 
--- octoprint_EnclosureFanController/static/js/EnclosureFanController.js
+++ octoprint_EnclosureFanController/static/js/EnclosureFanController.js
@@ -10,7 +10,7 @@ $(function() {
         // gets called _after_ the settings have been retrieved from the OctoPrint backend and thus
         // the SettingsViewModel been properly populated.
         self.onBeforeBinding = function() {
-	    self.enclosureTemp("N/A");
+		self.enclosureTemp("");
         };
 
 	self.onDataUpdaterPluginMessage = function(plugin, data) {
@@ -20,12 +20,15 @@ $(function() {
 
 		if (data.enclosureTemp){
 			var temperature = 0
-			temperature = data.enclosureTemp;
-			if (temperature == 0){
-				temperature = "N/A";
-			}
+			temperature = parseFloat(data.enclosureTemp);
 
-			self.enclosureTemp("Enclosure: ". concat(temperature));
+			if (temperature > 0){
+				self.enclosureTemp("Enclosure: " +  sprintf("%.1f&deg;F", temperature));
+			}
+			else
+			{
+				self.enclosurerTemp("N/A");
+			}
 		}
 	};
     }
@@ -33,15 +36,8 @@ $(function() {
     // This is how our plugin registers itself with the application, by adding some configuration
     // information to the global variable OCTOPRINT_VIEWMODELS
     OCTOPRINT_VIEWMODELS.push([
-        // This is the constructor to call for instantiating the plugin
-        EnclosureFanControllerViewModel,
-
-        // This is a list of dependencies to inject into the plugin, the order which you request
-        // here is the order in which the dependencies will be injected into your view model upon
-        // instantiation via the parameters argument
-        ["settingsViewModel"],
-
-        // Finally, this is the list of selectors for all elements we want this view model to be bound to.
-        ["#tab_plugin_EnclosureFanController", "#navbar_plugin_EnclosureFanController"]
+	EnclosureFanControllerViewModel,
+	["settingsViewModel"],
+	["#tab_plugin_EnclosureFanController","#settings_plugin_EnclosureFanController","#navbar_plugin_EnclosureFanController"]
     ]);
 });
